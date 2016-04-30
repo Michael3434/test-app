ef46a4e2f533e4cf1164abb8b9f335a49eec65e0
Michael monin <mt.monin@gmail.com>
2016-04-30 19:05:40 +0200

    fix landing

--- app/assets/javascripts/app.pages.js.coffee
+++ app/assets/javascripts/app.pages.js.coffee
@@ -3,6 +3,8 @@ app.pages =
     init: ->
       $("#scroll-down").click this.scrollHeroClicked
       $(".btn-cta").click this.scrollHeroClicked
+      $('.nav-testimonials').click this.scrollToTesimonial
+      $('.nav-cities').click this.scrollHeroClicked
       $("#video-modal").on "hidden.bs.modal", => @stopVideo()
     stopVideo: ->
       youtubeSrc = $('#ytplayer').attr("src");
@@ -11,6 +13,10 @@ app.pages =
     scrollHeroClicked: ->
       $("html, body").animate {
         scrollTop: $(".cities").offset().top
+      }, 750
+    scrollToTesimonial: ->
+      $("html, body").animate {
+        scrollTop: $(".testimonial").offset().top
       }, 750
 
 $(document).on "ready page:load", ->
--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -2,4 +2,31 @@ module HomeHelper
   def cities
     ["Bordeaux", "Grenoble", "Lille", "Lyon", "Marseille", "Montpellier", "Nantes", "Paris", "Rennes", "Strasbourg", "Toulouse"]
   end
+
+  def facebook_url(city)
+    case city
+    when "Paris"
+      "https://www.facebook.com/groups/760907487298386/"
+    when "Bordeaux"
+      "https://www.facebook.com/groups/812857502152075/"
+    when "Grenoble"
+      "https://www.facebook.com/groups/1717543308514315/"
+    when "Lille"
+      "https://www.facebook.com/groups/500782700122027/"
+    when "Lyon"
+      "https://www.facebook.com/groups/1542300126071634/"
+    when "Marseille"
+      "https://www.facebook.com/groups/254492064896049/"
+    when "Montpellier"
+      "https://www.facebook.com/groups/1018822214871099/"
+    when "Nantes"
+      "https://www.facebook.com/groups/621741371326238/"
+    when "Rennes"
+      "https://www.facebook.com/groups/35162264829516/"
+    when "Strasbourg"
+      "https://www.facebook.com/groups/1542943339334782/"
+    when "Toulouse"
+      "https://www.facebook.com/groups/978940688825892/"
+    end
+  end
 end
